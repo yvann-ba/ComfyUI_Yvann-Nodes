02329d3664c9afde8948c62f217b168c708b14bb
Yvann <barbot.yvann@gmail.com>
2024-09-07 22:35:55 +0200

    push new version, for early tester

--- node_configs.py
+++ node_configs.py
@@ -19,7 +19,7 @@ NODE_CONFIGS = {}
 add_node_config("Audio_Analysis_Yvann", {
     "BASE_DESCRIPTION": """
 
-#### Analyzes video audio to generate audio-reactive weights and masks
+### Analyzes video audio to generate audio-reactive weights and masks
 
 #### Parameters:
 - `smoothing_factor`: Smooths audio weights (higher values = smoother transitions)
@@ -32,14 +32,14 @@ add_node_config("Audio_Analysis_Yvann", {
 #### Outputs:
 - `Audio`: Same as input
 - `Audio Weights`: Audio-reactive float values for each frame, usable with IPAdapter, CN, etc.
-- `Audio Masks`: Audio weights converted to masks usable with any mask input: Latent Noise Mask, CN, IPadapter etc..
-- `Weights Graph`: Visual Graph of "Audio Weights", help to adjust smoothing_factor/global_intensity
+- `Audio Masks`: Audio weights converted to masks usable withLatent NoiseMask, CN, IPadapter...
+- `Weights Graph`: Visual Graph of "Audio Weights"
 """
 })
 
 add_node_config("Audio_Drums_Analysis_Yvann", {
     "BASE_DESCRIPTION": """
-#### Isolates and analyzes drums sounds in video audio to generate drum-reactive weights and masks
+### Isolates and analyzes drums sounds in video audio to generate drum-reactive weights and masks
 
 #### Parameters:
 - `smoothing_factor`: Smooths drums audio weights (higher values = smoother transitions)
@@ -51,16 +51,16 @@ add_node_config("Audio_Drums_Analysis_Yvann", {
 
 #### Outputs:
 - `Drums Audio`: Isolated drums audio from input
-- `Drums Weights`: Drums-reactive float values for each frame, usable with IPAdapter, CN, etc.
-- `Drums Masks`: Drums weights converted to masks usable with any mask input: Latent Noise Mask, CN, IPadapter etc..
-- `Weights Graph`: Visual Graph of "Drums Weights",help to adjust smoothing_factor/global_intensity
+- `Drums Weights`: Drums-reactive float values for each frame, usable with IPAdapter, CN...
+- `Drums Masks`: Drums weights converted to masks usable with LatentNoiseMask, CN, IPadapter...
+- `Weights Graph`: Visual Graph of "Drums Weights"
 """
 })
 
 add_node_config("Audio_Vocals_Analysis_Yvann", {
     "BASE_DESCRIPTION": """
     
-#### Isolates and analyzes vocal sounds in video audio to generate vocal-reactive weights and masks
+### Isolates and analyzes vocal sounds in video audio to generate vocal-reactive weights and masks
 
 #### Parameters:
 - `smoothing_factor`: Smooths vocals audio weights (higher values = smoother transitions)
@@ -72,8 +72,8 @@ add_node_config("Audio_Vocals_Analysis_Yvann", {
 
 #### Outputs:
 - `Vocals Audio`: Isolated vocals audio from input
-- `Vocals Weights`: Vocals-reactive float values for each frame, usable with IPAdapter, CN, etc.
-- `Vocals Masks`: Vocals weights converted to masks usable with any mask input: Latent Noise Mask, CN, IPadapter etc..
-- `Weights Graph`: Visual Graph of "Vocals Weights", help to adjust smoothing_factor/global_intensity
+- `Vocals Weights`: Vocals-reactive float values for each frame, usable with IPAdapter, CN...
+- `Vocals Masks`: Vocals weights converted to masks usable with LatentNoiseMask, CN, IPadapter...
+- `Weights Graph`: Visual Graph of "Vocals Weights"
 """
 })
--- nodes/audio/Audio_Analysis_Yvann.py
+++ nodes/audio/Audio_Analysis_Yvann.py
@@ -5,16 +5,11 @@ import tempfile
 import numpy as np
 from PIL import Image
 from ... import Yvann
-import random
 
 class AudioNodeBase(Yvann):
     CATEGORY= "👁️ Yvann Nodes/🔊 Audio"
         
 class Audio_Analysis_Yvann(AudioNodeBase):
-    def __init__(self):
-        self.output_dir = folder_paths.get_temp_directory()
-        self.type = "temp"
-        self.prefix_append = "_temp_" + ''.join(random.choice("abcdefghijklmnopqrstupvxyz") for x in range(5))
     @classmethod
     def INPUT_TYPES(cls):
         return {
@@ -36,14 +31,22 @@ class Audio_Analysis_Yvann(AudioNodeBase):
         return waveform[..., start:end].cpu().numpy().squeeze()
 
     def _rms_energy(self, waveform, num_frames, samples_per_frame):
-        return np.array([np.sqrt(np.mean(self._get_audio_frame(waveform, i, samples_per_frame)**2)) for i in range(num_frames)])
+        try:
+            return np.array([np.sqrt(np.mean(self._get_audio_frame(waveform, i, samples_per_frame)**2)) for i in range(num_frames)])
+        except Exception as e:
+            print(f"Error in RMS energy(Audio Anaalysis algorithm) calculation: {e}")
+            return np.zeros(num_frames)
 
     def _smooth_weights(self, weights, smoothing_factor):
-        if smoothing_factor <= 0.01:
+        try:
+            if smoothing_factor <= 0.01:
+                return weights
+            kernel_size = max(3, int(smoothing_factor * 50))
+            kernel = np.ones(kernel_size) / kernel_size
+            return np.convolve(weights, kernel, mode='same')
+        except Exception as e:
+            print(f"Error in smoothing weights: {e}")
             return weights
-        kernel_size = max(3, int(smoothing_factor * 50))  # Ensure minimum kernel size of 3
-        kernel = np.ones(kernel_size) / kernel_size
-        return np.convolve(weights, kernel, mode='same')
 
     def _normalize_weights(self, weights):
         min_val, max_val = np.min(weights), np.max(weights)
@@ -76,19 +79,35 @@ class Audio_Analysis_Yvann(AudioNodeBase):
         return masks_out
 
     def process_audio(self, audio, video_frames, smoothing_factor, global_intensity, prompt=None, filename_prefix="ComfyUI", extra_pnginfo=None):
+        
+        if audio is None:
+            print("No audio provided")
+            return (None, None, None, None,)
+        
         waveform = audio['waveform']
         sample_rate = audio['sample_rate']
 
+        if video_frames is None or not isinstance(video_frames, torch.Tensor) or video_frames.dim() != 4:
+            print("Invalid video frames input")
+            return (None, None, None, None,)
+        
+        if not isinstance(waveform, torch.Tensor):
+            print("Waveform is not a torch.Tensor")
+            return (None, None, None, None,)
+        
         num_frames, height, width, _ = video_frames.shape
 
-        if waveform.dim() == 3:
-            waveform = waveform.squeeze(0) 
+        # Ensure waveform is 3D (batch, channels, samples)
         if waveform.dim() == 1:
-            waveform = waveform.unsqueeze(0)  # Add channel dimension if mono
-        if waveform.shape[0] != 2:
-            waveform = waveform.repeat(2, 1)  # Duplicate mono to stereo if necessary
+            waveform = waveform.unsqueeze(0).unsqueeze(0)
+        elif waveform.dim() == 2:
+            waveform = waveform.unsqueeze(0)
 
         total_samples = waveform.shape[-1]
+        if total_samples < num_frames:
+            print("Audio is shorter than video")
+            return None, None, None, None
+
         samples_per_frame = total_samples // num_frames
 
         processed_audio = {
@@ -97,38 +116,47 @@ class Audio_Analysis_Yvann(AudioNodeBase):
         }
         
         audio_weights = self._rms_energy(waveform, num_frames, samples_per_frame)
+        if np.isnan(audio_weights).any() or np.isinf(audio_weights).any():
+            print("Invalid audio weights calculated")
+            return None, None, None, None
+
         audio_weights = self._smooth_weights(audio_weights, max(0.01, smoothing_factor))
         audio_weights = self._normalize_weights(audio_weights)
-        audio_weights = [round(float(weight), 3) for weight in audio_weights]
+        if len(audio_weights) != num_frames:
+            print("Mismatch between audio weights and video frames")
+            return None, None, None, None
 
         audio_weights = self.adjust_weights(np.array(audio_weights), global_intensity)
 
         # Plot the weights
         frames = list(range(1, num_frames + 1))
-        plt.figure(figsize=(10, 6))
-        plt.plot(frames, audio_weights, label='Audio Weights', color='blue')
-        plt.xlabel('Frame Number')
-        plt.ylabel('Normalized Weights')
-        plt.title('Normalized Weights for Audio (RMS Energy)')
-        plt.legend()
-        plt.grid(True)
+        try:
+            plt.figure(figsize=(10, 6))
+            plt.plot(frames, audio_weights, label='Audio Weights', color='blue')
+            plt.xlabel('Frame Number')
+            plt.ylabel('Normalized Weights')
+            plt.title('Normalized Weights for Audio (RMS Energy)')
+            plt.legend()
+            plt.grid(True)
 
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmpfile:
-            plt.savefig(tmpfile, format='png')
-            tmpfile_path = tmpfile.name
-        plt.close()
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmpfile:
+                plt.savefig(tmpfile, format='png')
+                tmpfile_path = tmpfile.name
+            plt.close()
 
-        weights_graph = Image.open(tmpfile_path).convert("RGB")
-        weights_graph = np.array(weights_graph)
-        weights_graph = torch.tensor(weights_graph).permute(2, 0, 1).unsqueeze(0).float() / 255.0
-        weights_graph = weights_graph.permute(0, 2, 3, 1)
+            weights_graph = Image.open(tmpfile_path).convert("RGB")
+            weights_graph = np.array(weights_graph)
+            weights_graph = torch.tensor(weights_graph).permute(2, 0, 1).unsqueeze(0).float() / 255.0
+            weights_graph = weights_graph.permute(0, 2, 3, 1)
+        except Exception as e:
+            print(f"Error in creating weights graph: {e}")
+            weights_graph = None
 
         # Generate masks from audio weights
         audio_masks = self.generate_masks(audio_weights, width, height)
 
-        return (
-            processed_audio,
-            audio_weights.tolist(),
-            audio_masks,
-            weights_graph,
-        )
+        if processed_audio is None or audio_weights is None or audio_masks is None or weights_graph is None:
+            print("One or more outputs are invalid")
+            return None, None, None, None
+
+        return processed_audio, audio_weights.tolist(), audio_masks, weights_graph
