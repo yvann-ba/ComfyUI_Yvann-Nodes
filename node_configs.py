9e2af1b83b193ea24b35a2089a37d7dde0b7e86b
Yvann <barbot.yvann@gmail.com>
2024-11-11 11:02:33 +0100

    test readme update and a dd docs config nodes

--- node_configs.py
+++ node_configs.py
@@ -16,6 +16,52 @@ def add_node_config(node_name, config):
 
 NODE_CONFIGS = {}
 
+add_node_config("LoadAudioSeparationModel", {
+    "BASE_DESCRIPTION": """
+Downloads and loads audio separation model. If the model is not already available, it will be downloaded to ComfyUI/models/audio_separation_model/
+
+
+**Parameter:**
+
+- **model**: Audio Separation model to load, [HybridDemucs](https://github.com/facebookresearch/demucs) is the most accurate, fatest and lightweight, [OpenUnmix](https://github.com/sigsep/open-unmix-pytorch) is an alternative
+
+**Output**:
+
+- **audio_separation_model**: Loaded audio separation model, connect it to "Audio Analysis" or "Audio Remixer" Nodes
+
+"""
+})
+
+add_node_config("AudioAnalysis", {
+    "BASE_DESCRIPTION": """
+Analyzes audio input to generate audio-reactive weights and visualizations.\n
+It can extract specific elements from the audio, such as drums, vocals, bass, or analyze the full audio.\n
+AI audio separator preprocess the audio, parameters allow manual control over the audio weights
+
+**Inputs:**
+
+- **audio_separation_model**: Load model from "Load Audio Separation Model" Node, currently support HybridDemucs and Open-Unmix
+- **audio**: Input audio file
+- **batch_size**: Number of frames to associates audio weights with
+- **fps**: Frames per second for processing audio weights
+
+**Parameters:**
+
+- **analysis_mode**: Selects the audio component to analyze
+- **threshold**: Only weights detected in the audio above this value pass through
+- **multiply**: Amplifies the weights by this factor, applied before normalization
+
+**Outputs:**
+
+- **graph_audio**: Image displaying a graph of the audio weights over each frames
+- **processed_audio**: The separated or processed audio (e.g., drums, vocals) used in the analysis
+- **original_audio**: The original audio input without modifications
+- **audio_weights**: A list of audio-reactive float weights based on the processed audio
+"""
+})
+
+#Audio Peaks Detection here
+
 add_node_config("AudioIPAdapterTransitions", {
     "BASE_DESCRIPTION": """
 Receives "peaks_weights" from "Audio Peaks Detection" Node to control blending and switching between images based on audio peaks.\n
@@ -44,35 +90,6 @@ Returns images and associated weights to use with two IPAdapter batches, inspire
 })
 
 
-add_node_config("AudioAnalysis", {
-    "BASE_DESCRIPTION": """
-Analyzes audio input to generate audio-reactive weights and visualizations.\n
-It can extract specific elements from the audio, such as drums, vocals, bass, or analyze the full audio.\n
-Using AI-based audio separator [open-unmix](https://github.com/sigsep/open-unmix-pytorch), the parameters allow manual control over the audio weights.
-
-**Inputs:**
-
-- **audio_separation_model**: Load model from "Load Audio Separation Model" Node, currently support HybridDemucs and Open-Unmix
-- **audio**: Input audio file
-- **batch_size**: Number of frames to associates audio weights with
-- **fps**: Frames per second for processing audio weights
-
-**Parameters:**
-
-- **analysis_mode**: Selects the audio component to analyze
-- **threshold**: Only weights detected in the audio above this value pass through
-- **multiply**: Amplifies the weights by this factor, applied before normalization
-
-**Outputs:**
-
-- **graph_audio**: Image displaying a graph of the audio weights over each frames
-- **processed_audio**: The separated or processed audio (e.g., drums, vocals) used in the analysis
-- **original_audio**: The original audio input without modifications
-- **audio_weights**: A list of audio-reactive float weights based on the processed audio
-"""
-})
-
-
 add_node_config("AudioPromptSchedule", {
     "BASE_DESCRIPTION": """
 Associates input prompts with floats into a scheduled prompt format.\n
@@ -90,22 +107,31 @@ Make sure to include an empty lines between each differents prompts
 """
 })
 
-add_node_config("FloatsToWeightsStrategy", {
+#Audio AnimateDiff Schedule here
+
+add_node_config("AudioRemixer", {
     "BASE_DESCRIPTION": """
-Converts a list of floats into an IPAdapter weights strategy.\n
-Use with "IPAdapter Weights From Strategy" or "Prompt Schedule From Weights Strategy" to pass audio weights or any float list to the IPAdapter pipeline.
+Enables modification of the input audio by adjusting the intensity of drums, bass, vocals, or other elements. The output is the audio with the applied modifications
 
 **Inputs:**
 
-- **floats**: List of float values to convert into a weights strategy
+- **audio_separation_model**: Load model from "Load Audio Separation Model" Node, currently support HybridDemucs and Open-Unmix
+- **audio**: The audio file you want to modify. You can load either mono or stereo format
 
 **Parameters:**
 
-**Outputs:**
+- **bass_volume**: Adjusts bass volume (-10 to mute, 10 = max value to amplify)
+- **drums_volume**: Adjusts drums volume
+- **others_volume**: Adjusts volume of others elements
+- **vocals_volume**: Adjusts vocals volume
 
-- **WEIGHTS_STRATEGY**: Dictionary containing the weights strategy for IPAdapter
+**Outputs**:
+
+- **merged_audio**: A composition of four separated tracks (drums, bass, vocals, other), modified as specified
+
 """
 })
+#Repeat ImagesToCount here 
 
 add_node_config("InvertFloats", {
     "BASE_DESCRIPTION": """
@@ -155,5 +181,23 @@ Converts mask inputs into float values by computing the mean pixel value of each
 **Outputs:**
 
 - **float**: Float representing the average value of the mask
+"""
+})
+
+
+add_node_config("FloatsToWeightsStrategy", {
+    "BASE_DESCRIPTION": """
+Converts a list of floats into an IPAdapter weights strategy.\n
+Use with "IPAdapter Weights From Strategy" or "Prompt Schedule From Weights Strategy" to pass audio weights or any float list to the IPAdapter pipeline.
+
+**Inputs:**
+
+- **floats**: List of float values to convert into a weights strategy
+
+**Parameters:**
+
+**Outputs:**
+
+- **WEIGHTS_STRATEGY**: Dictionary containing the weights strategy for IPAdapter
 """
 })
